f0063bfca02889a520c5be588d07e01cf798df73
jona2255 <jona2255@gmail.com>
2018-03-05 11:11:02 +0100

    falta finalizar contactos

--- sticky.js
+++ sticky.js
@@ -1,12 +1,16 @@
-window.onscroll = function() {myFunction()};
+window.onload = function() {
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+  window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
   }
+
 }
